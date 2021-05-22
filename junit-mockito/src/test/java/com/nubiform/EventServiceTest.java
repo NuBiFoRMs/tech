fb7126cee54559d38540d2a698f2eab83745440d
NuBiFoRM <nubiform@me.com>
2021-05-22 22:56:05 +0900

    mocking

--- junit-mockito/src/test/java/com/nubiform/EventServiceTest.java
+++ junit-mockito/src/test/java/com/nubiform/EventServiceTest.java
@@ -2,17 +2,26 @@ package com.nubiform;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 class EventServiceTest {
+
+    EmailSender emailSender = Mockito.mock(EmailSender.class);
 
     EventService eventService;
 
     @BeforeEach
     public void setUp() {
+        when(emailSender.send(anyString())).thenReturn(true);
+
         // arrange
-        eventService = new EventService(new EmailSender());
+        eventService = new EventService(emailSender);
     }
 
     @Test
